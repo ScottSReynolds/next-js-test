0bab050565ddda15b1d1257a4991613676eebe8f
Scott Reynolds <scottsreynolds@gmail.com>
2023-05-20 16:50:35 -0500

    asdf

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const repo = 'next-js-test'
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-module.exports = nextConfig
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
